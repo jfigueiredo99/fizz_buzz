6643ee6c6e24f88c202bf0e761cf4c13e07a5d51
Joao Figueiredo <joaoricardo1999@gmail.com>
2022-08-16 22:52:00 +0100

    Added FizzBuzz Rules

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// RULES
+
+// Print integers 1 to N,
+// but print “Fizz” if an integer is divisible by 3,
+// “Buzz” if an integer is divisible by 5,
+// and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+using System;
 
 Console.WriteLine("Hello, World!");
