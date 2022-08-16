d5f5d39ee5be5e70d2e7249676e4f2202c20f50f
Andre Ramos <aluno20710@ipt.pt>
2022-08-16 23:29:30 +0100

    Procedural Implementation

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,4 +7,25 @@
 
 using System;
 
-Console.WriteLine("Hello, World!");
+
+for ( int i = 1; i <= 50; i++)
+{
+   
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    
+}
