8cc0e3a1f5b0afbde617369a7b376241a61de781
Joao Figueiredo <joaoricardo1999@gmail.com>
2022-08-16 23:32:11 +0100

    Added new Woof Rule

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,6 +5,8 @@
 // “Buzz” if an integer is divisible by 5,
 // and “FizzBuzz” if an integer is divisible by both 3 and 5.
 
+// NEW RULE If an integer is divisible by 7 print “Woof”,
+
 using System;
 
 
