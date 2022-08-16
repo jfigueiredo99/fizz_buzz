06c02dc844b2e519768be9a157bd648b6fb244c4
Joao Figueiredo <joaoricardo1999@gmail.com>
2022-08-16 23:36:09 +0100

    Better Woof Rule

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,7 +5,12 @@
 // “Buzz” if an integer is divisible by 5,
 // and “FizzBuzz” if an integer is divisible by both 3 and 5.
 
-// NEW RULE If an integer is divisible by 7 print “Woof”,
+// NEW RULES
+// Print integers 50 to 110,
+// If an integer is divisible by 7 print “Woof”,
+// If an integer is divisible by both 3 and 7 prints “FizzWoof”,
+// If an integer is divisible by both 5 and 7 prints “BuzzWoof,
+// If an integer is divisible by 3,5 and 7 prints “FizzBuzzWoof.
 
 using System;
 
